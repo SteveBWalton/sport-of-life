6debd50189c75f3f9b92a76a19a43fad77fbb9d0
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2018-04-16 15:30:01 +0100

    This reports the operating system and displays some red text under Linux.  I expect that it does not work under windows.

---
+++ sport_of_life.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Module to run the sport of life program.
+This is a little program to test console control under Linux and Windows.
+'''
+
+# System libraries.
+import sys
+import os
+import argparse
+import platform
+import subprocess
+import shutil
+import datetime
+
+# Application Libraries.
+
+
+
+if __name__ == '__main__':
+    # Process the command line arguments.
+    # This might end the program (--help).
+    oParse = argparse.ArgumentParser(prog='sport_of_life', description='Little game to run under Linux and Windows.')
+    oArgs = oParse.parse_args()
+
+    # Welcome message.
+    print('\033[1;31mSport Of Life\033[0;m by Steve Walton.')
+    print('Python Version {}.{}.{} (expecting Python 3).'.format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
+    print('Operating System is "{}".  Desktop is "{}".'.format(platform.system(), os.environ.get('DESKTOP_SESSION')))
+
+    # Main loop.
+
+    print('Goodbye from the \033[1;31mSport Of Life\033[0;m program.')
